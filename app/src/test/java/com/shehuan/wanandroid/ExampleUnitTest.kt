8c302ec86fddb5deb3b5f0c6c244f6250699dc8d
huangcong <huangcong@360.cn>
2018-12-06 16:02:53 +0800

    Test: Factorial opt

--- app/src/test/java/com/shehuan/wanandroid/ExampleUnitTest.kt
+++ app/src/test/java/com/shehuan/wanandroid/ExampleUnitTest.kt
@@ -5,6 +5,7 @@ import io.reactivex.functions.Consumer
 import org.junit.Test
 import org.junit.jupiter.api.BeforeEach
 import java.io.File
+import java.math.BigDecimal
 import kotlin.test.assertEquals
 
 /**
@@ -24,7 +25,7 @@ class ExampleUnitTest {
     }
 
     @Test
-    fun main() {
+    fun countLetter() {
         var text = File(ClassLoader.getSystemResource("input")?.path).readText()
         Observable.fromIterable(text.toCharArray().asIterable())
                 //.filter { !it.isWhitespace() }
@@ -33,6 +34,33 @@ class ExampleUnitTest {
                     o.count().subscribe(Consumer { println("${o.key} -> $it") })
                 }
     }
+
+    object Result {
+        var bigDecimal: BigDecimal = BigDecimal(1)
+    }
+
+    /**
+     * 阶乘
+     *
+     * -> tailrec -> 尾递归优化
+     */
+    private tailrec fun factorial(start: Int, end: Int, result: Result) {
+        when {
+            start <= 0 ||
+                    start == end -> return
+        }
+        result.bigDecimal = result.bigDecimal.multiply(BigDecimal(start))
+        factorial(start + 1, end, result)
+    }
+
+    @Test
+    fun testFactorial() {
+        val result = Result
+        factorial(1, 1000000, result)
+        println(result.bigDecimal)
+    }
+
+
 }
 
 
