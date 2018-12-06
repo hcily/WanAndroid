7cfa9f1ff4d7de106a1b98aaf15983a47227220a
huangcong <huangcong@360.cn>
2018-12-06 15:26:54 +0800

    Test: Count letters

--- app/src/test/java/com/shehuan/wanandroid/ExampleUnitTest.kt
+++ app/src/test/java/com/shehuan/wanandroid/ExampleUnitTest.kt
@@ -1,7 +1,9 @@
 package com.shehuan.wanandroid
 
 import io.reactivex.Observable
+import io.reactivex.functions.Consumer
 import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
 import java.io.File
 import kotlin.test.assertEquals
 
@@ -11,17 +13,24 @@ import kotlin.test.assertEquals
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @BeforeEach
+    internal fun setUp() {
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
 
     @Test
-    fun main(args: Array<String>) {
-        var text = File(ClassLoader.getSystemResource("input").path).readText()
-        Observable.fromIterable(text.toCharArray().asIterable()).filter { it.isWhitespace() }
-                .subscribe {
-                    println(it)
+    fun main() {
+        var text = File(ClassLoader.getSystemResource("input")?.path).readText()
+        Observable.fromIterable(text.toCharArray().asIterable())
+                //.filter { !it.isWhitespace() }
+                .groupBy { it }
+                .subscribe { o ->
+                    o.count().subscribe(Consumer { println("${o.key} -> $it") })
                 }
     }
 }
